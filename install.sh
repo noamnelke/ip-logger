59eb6acc6671c90ed15c570f2f9a1bc64a5664c4
Noam Nelke <noamnelke@gmail.com>
2022-02-23 10:10:50 +0200

    conform to Debian cron script namespace

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@ read -p "Run on network connetion? [Y/n]" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]?$ ]]
 then
-	echo $SCRIPT > /etc/network/if-up.d/log_ip.sh
-	chmod +x /etc/network/if-up.d/log_ip.sh
+	echo $SCRIPT > /etc/network/if-up.d/log-ip
+	chmod +x /etc/network/if-up.d/log-ip
 fi
 
 read -p "Run hourly? [Y/n]" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]?$ ]]
 then
-	echo $SCRIPT > /etc/cron.hourly/log_ip.sh
-	chmod +x /etc/cron.hourly/log_ip.sh
+	echo $SCRIPT > /etc/cron.hourly/log-ip
+	chmod +x /etc/cron.hourly/log-ip
 fi
